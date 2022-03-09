5d2c242e2bcef51247ba344dcafbdf622887d3ff
Marcio H <marcio.o@github.com>
2022-03-09 16:32:09 -0300

    migration: tbcastracao - correcao funcao down

--- src/database/migrations/20220309185758-tbcastracao.js
+++ src/database/migrations/20220309185758-tbcastracao.js
@@ -25,6 +25,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    return await queryInterface.dropTable('users');
+    return await queryInterface.dropTable('tbcastracao');
   }
 };
